bcf975ebfc55a19bd200c3c445e8e229667f219a
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-04-10 15:50:33 -0300

    wsl-config.ps1

---
+++ wsl-config.ps1
@@ -0,0 +1,7 @@
+echo "[wsl2]" >> $ENV:USERPROFILE/.wslconfig
+echo "processors=2" >> $ENV:USERPROFILE/.wslconfig
+echo "memory=1GB" >> $ENV:USERPROFILE/.wslconfig
+echo "swap=1GB" >> $ENV:USERPROFILE/.wslconfig
+echo "localhostForwarding=true" >> $ENV:USERPROFILE/.wslconfig
+echo "guiApplications=false" >> $ENV:USERPROFILE/.wslconfig
+
